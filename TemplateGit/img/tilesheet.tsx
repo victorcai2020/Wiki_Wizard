64d59600c8475515a53e9ed9608c8e9aa889cb56
victorcai2020 <62484134+victorcai2020@users.noreply.github.com>
2021-09-03 20:41:42 -0400

    initial commit

---
+++ TemplateGit/img/tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tilemap" tilewidth="64" tileheight="64" tilecount="480" columns="30">
+ <image source="tilemap.png" width="1920" height="1080"/>
+</tileset>
